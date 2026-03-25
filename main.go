a48a1018b24294227ff03fd3e9f33ff6a0835f38
amroalaa <amroalaa143@gmail.com>
2026-03-25 04:33:18 +0200

    تنظيم المشاريع في مجلدات فرعية وتحديث الروابط

--- main.go
+++ main.go
@@ -14,17 +14,18 @@ type project struct {
 }
 
 func main() {
+	baseRepoURL := "https://github.com/amroalaa"
 	projects := []project{
-		{1, "الآلة الحاسبة", "المتغيرات و Switch", "https://github.com/amroalaa"},
-		{2, "تخمين الرقم", "الحلقات التكرارية (For)", "https://github.com/amroalaa"},
-		{3, "محول الوحدات", "العمليات الحسابية و Printf", "https://github.com/amroalaa"},
-		{4, "مولد الكلمات", "النصوص (Strings) و Slices", "https://github.com/amroalaa"},
-		{5, "قائمة المهام", "append و range", "https://github.com/amroalaa"},
-		{6, "محلل النصوص", "الخرائط (Maps)", "https://github.com/amroalaa"},
-		{7, "إدارة المكتبة", "الهياكل (Structs)", "https://github.com/amroalaa"},
-		{8, "خادم الويب", "حزمة net/http", "https://github.com/amroalaa"},
-		{9, "فحص المواقع", "التزامن (Goroutines)", "https://github.com/amroalaa"},
-		{10, "تطبيق الطقس", "JSON و APIs", "https://github.com/amroalaa"},
+		{1, "الآلة الحاسبة", "المتغيرات و Switch", baseRepoURL + "calculator"},
+		{2, "تخمين الرقم", "الحلقات التكرارية", baseRepoURL + "guessing-game"},
+		{3, "محول الوحدات", "العمليات الحسابية", baseRepoURL + "unit-converter"},
+		{4, "مولد الكلمات", "النصوص و Slices", baseRepoURL + "password-generator"},
+		{5, "قائمة المهام", "append و range", baseRepoURL + "todo-list"},
+		{6, "محلل النصوص", "الخرائط (Maps)", baseRepoURL + "word-counter"},
+		{7, "إدارة المكتبة", "الهياكل (Structs)", baseRepoURL + "library-system"},
+		{8, "خادم الويب", "حزمة net/http", baseRepoURL + "simple-web-server"},
+		{9, "فحص المواقع", "التزامن (Goroutines)", baseRepoURL + "site-checker"},
+		{10, "تطبيق الطقس", "JSON و APIs", baseRepoURL + "weather-cli"},
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		templ := template.Must(template.ParseFiles("index.html"))
