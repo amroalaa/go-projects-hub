f3afd3240e7c22c7bb428e6fb18d63a7b2c04fef
amroalaa <amroalaa143@gmail.com>
2026-03-25 04:24:32 +0200

    إضافة روابط المشاريع وتحديث التصميم

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package mmain
+package main
 
 import (
 	"html/template"
@@ -10,20 +10,21 @@ type project struct {
 	ID    int
 	Title string
 	Skill string
+	link  string
 }
 
 func main() {
 	projects := []project{
-		{1, "الآلة الحاسبة", "المتغيرات و Switch"},
-		{2, "تخمين الرقم", "الحلقات التكرارية (For)"},
-		{3, "محول الوحدات", "العمليات الحسابية و Printf"},
-		{4, "مولد الكلمات", "النصوص (Strings) و Slices"},
-		{5, "قائمة المهام", "append و range"},
-		{6, "محلل النصوص", "الخرائط (Maps)"},
-		{7, "إدارة المكتبة", "الهياكل (Structs)"},
-		{8, "خادم الويب", "حزمة net/http"},
-		{9, "فحص المواقع", "التزامن (Goroutines)"},
-		{10, "تطبيق الطقس", "JSON و APIs"},
+		{1, "الآلة الحاسبة", "المتغيرات و Switch", "https://github.com/amroalaa"},
+		{2, "تخمين الرقم", "الحلقات التكرارية (For)", "https://github.com/amroalaa"},
+		{3, "محول الوحدات", "العمليات الحسابية و Printf", "https://github.com/amroalaa"},
+		{4, "مولد الكلمات", "النصوص (Strings) و Slices", "https://github.com/amroalaa"},
+		{5, "قائمة المهام", "append و range", "https://github.com/amroalaa"},
+		{6, "محلل النصوص", "الخرائط (Maps)", "https://github.com/amroalaa"},
+		{7, "إدارة المكتبة", "الهياكل (Structs)", "https://github.com/amroalaa"},
+		{8, "خادم الويب", "حزمة net/http", "https://github.com/amroalaa"},
+		{9, "فحص المواقع", "التزامن (Goroutines)", "https://github.com/amroalaa"},
+		{10, "تطبيق الطقس", "JSON و APIs", "https://github.com/amroalaa"},
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		templ := template.Must(template.ParseFiles("index.html"))
